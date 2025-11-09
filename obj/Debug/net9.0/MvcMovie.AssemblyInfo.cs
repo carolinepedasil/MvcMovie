d651b48c9886500ef98661595160264cbc9d51d4
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2025-11-09 17:09:41 -0300

    Part 3, add a view to an ASP.NET Core MVC app

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23a95ca541076f66e68470b1c2fd94923567cdb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa4c74cc996ce944a31e04bda409f10458230cab")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
