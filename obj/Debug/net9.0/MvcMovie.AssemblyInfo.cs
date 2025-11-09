4a27a28de6db001f17ae6b600798f24feb387cc8
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2025-11-09 20:03:38 -0300

    Part 7, add search to an ASP.NET Core MVC app

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347e34a4da75cfb1fb1339a6e5de2233b3e12737")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aeafc3ab34d94c2aced7e265999d343449b7af2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
