9b126bfcb224cdd35ad75f7a8e2888cb2a69c513
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2025-11-09 20:22:07 -0300

    Part 8, add a new field to an ASP.NET Core MVC app

--- obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net9.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aeafc3ab34d94c2aced7e265999d343449b7af2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a27a28de6db001f17ae6b600798f24feb387cc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
